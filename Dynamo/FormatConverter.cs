9a859ab4814ea20ba13409fbb6eeda75645cd78f
LastKeyDev <117791770+LastKeyDev@users.noreply.github.com>
2024-06-14 14:48:40 -0300

    init

---
+++ Dynamo/FormatConverter.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Data;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.Win32;
+namespace Dynamo
+{
+    public class FormatConverter
+    {
+        DataTable dt = new DataTable();
+        public FormatConverter()
+        {
+            
+        }
+
+        public DataTable PopulateGrid()
+        {
+        
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel files (*.xls*, *.xlsx)|*.xls*; *.xlsx";
+            if (ofd.ShowDialog() == true)
+            {
+                string _sFileName = ofd.FileName;
+               string _sPath = Path.GetFullPath(ofd.FileName);
+                ReadExcel(_sPath, _sFileName, dt);
+            }
+            return dt;
+        }
+
+        public void ReadExcel(string path, string filename, DataTable dt)
+        {
+            
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(path, false))
+            {
+                WorkbookPart workbookPart = doc.WorkbookPart;
+                IEnumerable<Sheet> sheets = doc.WorkbookPart.Workbook.GetFirstChild<Sheets>().Elements<Sheet>();
+                string relationshipId = sheets.First().Id.Value;
+                WorksheetPart worksheetPart = (WorksheetPart)doc.WorkbookPart.GetPartById(relationshipId);
+                Worksheet workSheet = worksheetPart.Worksheet;
+                SheetData sheetData = workSheet.GetFirstChild<SheetData>();
+                IEnumerable<Row> rows = sheetData.Descendants<Row>();
+
+                foreach (Cell cell in rows.ElementAt(0))
+                {
+                    dt.Columns.Add(GetCellValue(doc, cell));
+                }
+
+                foreach (Row row in rows) //this will also include your header row...
+                {
+                    DataRow tempRow = dt.NewRow();
+
+                    for (int i = 0; i < row.Descendants<Cell>().Count(); i++)
+                    {
+                        tempRow[i] = GetCellValue(doc, row.Descendants<Cell>().ElementAt(i));
+                    }
+
+                    dt.Rows.Add(tempRow);
+                }
+
+            }
+            dt.Rows.RemoveAt(0);
+        }
+
+        public static string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            SharedStringTablePart stringTablePart = document.WorkbookPart.SharedStringTablePart;
+            string value = cell.CellValue == null? "" : cell.CellValue.InnerXml;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return stringTablePart.SharedStringTable.ChildElements[Int32.Parse(value)].InnerText;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+
+    }
+}
